82bc1cd47680ca6b013bf73306edbfe4fb0b9236
duicul <32304082+duicul@users.noreply.github.com>
2020-10-27 15:11:58 +0200

    uWSGI config file

--- WebVideoPlayer/Createuwsgiconfig.py
+++ WebVideoPlayer/Createuwsgiconfig.py
@@ -9,9 +9,11 @@ print(BASE_DIR)
 str_out="[uwsgi] \nchdir = "
 str_out+=BASE_DIR
 str_out+="\n"
-str_out+="env = DJANGO_SETTINGS_MODULE=project.settings.production # customize with your settings module \n"
-str_out+="wsgi-file = project/wsgi.py # customize with the relative path to your wsgi.py file \n"
 str_out+="plugins = python3\n"
+str_out+="module=WebVideoPlayer.wsgi:application\n"
+str_out+="env = 'DJANGO_SETTINGS_MODULE=WebVideoPlayer.settings' # customize with your settings module \n"
+str_out+="wsgi-file = WebVideoPlayer/wsgi.py # customize with the relative path to your wsgi.py file \n"
+
 str_out+="workers = 1"
 f = open("/etc/uwsgi/apps-enabled/django.ini", "w")
 f.write(str_out)
