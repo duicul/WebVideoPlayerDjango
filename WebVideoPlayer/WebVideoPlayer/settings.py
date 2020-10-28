721040b2f7f03c585a83faff97cd5287c43ca34d
duicul <duicul@yahoo.com>
2020-10-28 11:36:39 +0200

    keep debug false

--- WebVideoPlayer/WebVideoPlayer/settings.py
+++ WebVideoPlayer/WebVideoPlayer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm#-i!c((dr051bklq^l2iws+yif3@bw=b%z8ic9%vg3)up2y5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
