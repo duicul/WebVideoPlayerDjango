b668a3f05c92e3e4bf0535e597479e0b8a960c74
duicul <32304082+duicul@users.noreply.github.com>
2020-10-28 14:44:25 +0200

    change log to logs in setup.py

--- WebVideoPlayer/setup.py
+++ WebVideoPlayer/setup.py
@@ -5,7 +5,7 @@ Created on Oct 27, 2020
 '''
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-myCmd = 'mkdir '+BASE_DIR+'/log;chmod a+w '+BASE_DIR+'/log;'
+myCmd = 'mkdir '+BASE_DIR+'/logs;chmod a+w '+BASE_DIR+'/logs;'
 os.system(myCmd)
 myCmd = 'sudo apt install nginx uwsgi uwsgi-plugin-python3'
 os.system(myCmd)
