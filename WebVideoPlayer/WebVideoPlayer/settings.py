6a8da9aba8f1cac874550f5db2b45ad28b4553dc
duicul <32304082+duicul@users.noreply.github.com>
2020-10-28 21:57:46 +0200

    Add time to log

--- WebVideoPlayer/WebVideoPlayer/settings.py
+++ WebVideoPlayer/WebVideoPlayer/settings.py
@@ -158,7 +158,7 @@ LOGGING = {
  
     'formatters':{
         'Simple_Format':{
-            'format': '{levelname} {message}',
+            'format': '%(asctime)-15s {levelname} {message}',
             'style': '{',
         }
     },
