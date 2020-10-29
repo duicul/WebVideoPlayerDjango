00cf7e71e5f7080158bdfc4dd821937e7e64fb2a
duicul <32304082+duicul@users.noreply.github.com>
2020-10-29 19:10:39 +0200

    Add carousel

--- WebVideoPlayer/static/js/utils.js
+++ WebVideoPlayer/static/js/utils.js
@@ -28,26 +28,115 @@ function load_videos(){
    var url_list_dir="/utils/list_video_files";
    console.log(url_list_dir);
    $.ajax({url: url_list_dir, success: function(result){
+        let img_w=250;
+        let img_h=250;
+        movies_per_slide=Math.floor(window.innerWidth*0.9/img_w);
+        slides=Math.ceil(result.length/movies_per_slide)
+        console.log(movies_per_slide)
+        console.log(slides)
+        let ret_str="<div id=\"demo\" class=\"carousel slide\" data-ride=\"carousel\" style=\"background-color: lightblue;\">";
+        ret_str+="<ul class=\"carousel-indicators\">";
+        ret_str+="<li data-target=\"#demo\" data-slide-to=\"0\" class=\"active\"></li>";
+        var i;
+        for (i = 1; i < slides; i++) {
+            ret_str+="<li data-target=\"#demo\" data-slide-to=\""+i+"\"></li>";
+        } 
+        ret_str+="</ul>";
+        ret_str+="<div class=\"carousel-inner\">";
+            ret_str+="<div class=\"carousel-item active\" style=\"text-align: center\">";
+            for (i = 0; i < movies_per_slide; i++) {
+                ret_str+="<a href=\"/video_player/?play="+result[i]["movie_url"]+"\">";
+                ret_str+="<img style=\"border-radius: 8%;cursor: pointer;\" src=\""+result[i]["img_url"];
+                ret_str+="\" alt=\""+result[i]["name"]+"\" width=\""+img_w+"\" height=\""+img_h+"\">";
+                ret_str+="</a>";
+                ret_str+="<span> </span>";
+            } 
+            ret_str+="</div>";
+            var slide_i;
+            for (slide_i = 1; slide_i < slides;slide_i++) {
+            ret_str+="<div class=\"carousel-item\" style=\"text-align: center\">";
+                for (i = 0; i < movies_per_slide; i++) {
+                    current_index=slide_i*movies_per_slide+i;
+                    if(current_index>=result.length)
+                        break;
+                    ret_str+="<a href=\"/video_player/?play="+result[current_index]["movie_url"]+"\">";
+                    ret_str+="<img style=\"border-radius: 8%;cursor: pointer;\" src=\""+result[current_index]["img_url"];
+                    ret_str+="\" alt=\""+result[current_index]["name"]+"\" onclick=\"present_video('"+result[current_index]["movie_url"]+"');\" width=\""+img_w+"\" height=\""+img_h+"\">";
+                    ret_str+="</a>";
+                    ret_str+="<span>  </span>";
+                } 
+            ret_str+="</div>";
+            
+            }
+        ret_str+="</div>";
+        ret_str+="<a class=\"carousel-control-prev\" href=\"#demo\" data-slide=\"prev\">";
+        ret_str+="<span class=\"carousel-control-prev-icon\"></span></a>";
+        ret_str+="<a class=\"carousel-control-next\" href=\"#demo\" data-slide=\"next\">";
+        ret_str+="<span class=\"carousel-control-next-icon\"></span></a>";
+        ret_str+="</div>";
         console.log(result);
-        var ret_str="<div class=\"list-group\">";
+       /* var ret_str="<div class=\"list-group\">";
         var space_ind=0;
         console.log(Object.keys(result));
         result.forEach(function(item_par){
-            ret_str+="<div class=\"list-group-item\" onclick=\"present_video('"+item_par[1].replace("\\","/")+"');\" >"+item_par[0];
+        console.log(item_par["name"])
+            ret_str+="<div class=\"list-group-item\" onclick=\"present_video('"+item_par["movie_url"]+"');\" >";
+            ret_str+="<img src=\"";
+            ret_str+=item_par["img_url"];
+            ret_str+="\" alt=\""+item_par["name"]+"\" width=\"100\" height=\"100\">";
             ret_str+="</div>";
         }
         
         );
-        ret_str+="</div>";
+        ret_str+="</div>";*/
         $("#video_files").html(ret_str);
-   }
+   }//put multiple videos in one carousel item
    });
-    
+    /*
+    <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="la.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="chicago.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="ny.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+    */
     
 }
 
+
+
 function present_video(relative_path){
-    location.replace("/video_player?play=/media/"+relative_path);
+    location.replace("/video_player?play="+relative_path);
     /*let str_src="<video";
     str_src+="id=\"my-video\"";
     str_src+="class=\"video-js\"";
--- WebVideoPlayer/utils/path_walker.py
+++ WebVideoPlayer/utils/path_walker.py
@@ -12,13 +12,14 @@ def parse_dir(path):
     #print(path)
     t=[]
     main_path=path
+    #print(main_path)
     exten = ['.mp4','.avi','.m3u8']
     for dirpath, dirnames, files in os.walk(path,followlinks=True):
         for name in files:
             for ext in exten:
                 if name.lower().endswith(ext):
                     #print(name)
-                    print(dirpath)
+                    #print(dirpath)
                     #print(os.path.join(dirpath, name))
                     #t.append(os.path.join(dirpath, name))
                     out_path=os.path.join(dirpath, name)
@@ -27,7 +28,19 @@ def parse_dir(path):
                     #print(os.path.relpath(out_path,start=main_path))
                     #print(re.sub(r'^\.\\','',out_path))
                     #print()
-                    t.append([re.sub(r'^\.\\','',out_path),os.path.relpath(out_path,start=main_path)])
+                    main_moive_path=Path(out_path).resolve().parent.parent if ext == ".m3u8" else Path(out_path).resolve().parent
+                    img_path=os.path.join(main_moive_path,"poster.jpg")
+                    if not os.path.isfile(img_path):
+                        img_path="/static/img/not_found.jpg"
+                    else:
+                        img_path="/media/"+os.path.relpath(img_path,start=main_path).replace("\\","/")
+                    main_file_name=name.split(".")
+                    main_file_name=''.join(main_file_name[0:len(main_file_name)-1])
+                    #print(main_file_name)
+                    #print(img_path)
+                    new_entry={"name":main_file_name,"img_url":img_path,"movie_url":"/media/"+os.path.relpath(out_path,start=main_path).replace("\\","/")}
+                    #print(new_entry)
+                    t.append(new_entry)
     return t
 
 def parse_media_dir():
