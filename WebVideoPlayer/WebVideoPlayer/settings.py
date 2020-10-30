6e68c1179e2a695f30b2c06bb69c7527b36d42bc
duicul <32304082+duicul@users.noreply.github.com>
2020-10-30 14:54:14 +0200

    Add utils app to INSTALLED_APPS

--- WebVideoPlayer/WebVideoPlayer/settings.py
+++ WebVideoPlayer/WebVideoPlayer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'video_player.apps.VideoPlayerConfig',
+    'utils.apps.UtilsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
