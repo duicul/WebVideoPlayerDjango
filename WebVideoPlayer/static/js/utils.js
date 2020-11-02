f2019d32c7152c0f6ec15c6f5934b81c277d061f
duicul <32304082+duicul@users.noreply.github.com>
2020-11-02 17:59:40 +0200

    correct scan status

--- WebVideoPlayer/static/js/utils.js
+++ WebVideoPlayer/static/js/utils.js
@@ -184,10 +184,10 @@ function rescanfiles(){
     ready_str+="</svg>Scan</div>";
     
     let error_str="<div style=\"color:red;\">";
-    ready_str+="<svg width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" class=\"bi bi-arrow-clockwise\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">";
-    ready_str+="<path fill-rule=\"evenodd\" d=\"M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z\"/>";
-    ready_str+="<path d=\"M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z\"/>";
-    ready_str+="</svg>Scan error</div>";
+    error_str+="<svg width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" class=\"bi bi-arrow-clockwise\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">";
+    error_str+="<path fill-rule=\"evenodd\" d=\"M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z\"/>";
+    error_str+="<path d=\"M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z\"/>";
+    error_str+="</svg>Scan error</div>";
     
     $("#scan_status").html(wait_str);
     $.ajax({url: url_list_dir, success: function(result){
