6c8ec981612d03a4b0d8450bf77c487e96a5d7a5
duicul <32304082+duicul@users.noreply.github.com>
2020-11-06 19:32:20 +0200

    Prev and next episode

--- WebVideoPlayer/video_player/views.py
+++ WebVideoPlayer/video_player/views.py
@@ -39,6 +39,18 @@ def video(request):
                 return render(request,"main.html",{"movie_name":movie_name,"type":type,"play_src":play_src,"username":username,"subs":subs})
             elif type=="episode":
                 ep_db=Episode_db.objects.get(unique_id=uuid)
+                prv_ep=previous_ep(ep_db)
+                nxt_ep=next_ep(ep_db)
+                prv_ep_name=None
+                prv_ep_uuid=None
+                nxt_ep_name=None
+                nxt_ep_uuid=None
+                if prv_ep != None:
+                    prv_ep_name=prv_ep.name
+                    prv_ep_uuid=prv_ep.unique_id
+                if nxt_ep != None:
+                    nxt_ep_name=nxt_ep.name
+                    nxt_ep_uuid=nxt_ep.unique_id
                 play_src=ep_db.movie_url
                 subs=json.loads(ep_db.sub_json)
                 subs=[(sub,os.path.basename(sub)) for sub in subs]
@@ -46,7 +58,7 @@ def video(request):
                 season_url="/entry_point?type=season&uuid="+str(ep_db.season.unique_id)
                 episode_name=ep_db.name
                 show_name=ep_db.season.show.name
-                return render(request,"main.html",{"type":type,"play_src":play_src,"username":username,"subs":subs,"show_name":show_name,"episode_name":episode_name,"season_name":season_name,"season_url":season_url})
+                return render(request,"main.html",{"prv_ep_name":prv_ep_name,"prv_ep_uuid":prv_ep_uuid,"nxt_ep_name":nxt_ep_name,"nxt_ep_uuid":nxt_ep_uuid,"type":type,"play_src":play_src,"username":username,"subs":subs,"show_name":show_name,"episode_name":episode_name,"season_name":season_name,"season_url":season_url})
             else:
                 play_src=""
                 subs=[]
@@ -120,3 +132,79 @@ def redirect_internal(request,path):
     response['X-Accel-Redirect'] = '/media-internal/' + path
     logger.info("redirect_internal static = "+str(response['X-Accel-Redirect']))
     return response
+
+def previous_ep(ep):
+    curr_season=ep.season
+    season_eps=Episode_db.objects.all().filter(season=curr_season.pk)
+    prev_episode=None
+    curr_episode_ind=None
+    
+    for episode in season_eps:
+        if episode.abs_path==ep.abs_path:
+            curr_episode_ind=prev_episode
+            if prev_episode!=None:
+                return curr_episode_ind
+            break
+        prev_episode=episode
+    
+    
+    if curr_episode_ind == None and prev_episode!=None:
+        return None
+    
+    curr_show=curr_season.show
+    show_seasons=Season_db.objects.all().filter(show=curr_show.pk)
+    prev_season=None
+    curr_season_ind=None
+    for season in show_seasons:
+        if season.abs_path==curr_season.abs_path:
+            curr_season_ind=prev_season
+            break
+        prev_season=season
+    
+    if curr_season_ind==None:
+        return None
+    
+    season_eps=Episode_db.objects.all().filter(season=curr_season_ind.pk)
+    for episode in season_eps:
+        if episode.abs_path==ep.abs_path:
+            return curr_episode_ind
+    
+    return None
+    
+def next_ep(ep):
+    curr_season=ep.season
+    season_eps=Episode_db.objects.all().filter(season=curr_season.pk).order_by("-abs_path")
+    prev_episode=None
+    curr_episode_ind=None
+    
+    for episode in season_eps:
+        if episode.abs_path==ep.abs_path:
+            curr_episode_ind=prev_episode
+            if prev_episode!=None:
+                return curr_episode_ind
+            break
+        prev_episode=episode
+    
+    
+    if curr_episode_ind == None and prev_episode!=None:
+        return None
+    
+    curr_show=curr_season.show
+    show_seasons=Season_db.objects.all().filter(show=curr_show.pk).order_by("-abs_path")
+    prev_season=None
+    curr_season_ind=None
+    for season in show_seasons:
+        if season.abs_path==curr_season.abs_path:
+            curr_season_ind=prev_season
+            break
+        prev_season=season
+    
+    if curr_season_ind==None:
+        return None
+    
+    season_eps=Episode_db.objects.all().filter(season=curr_season_ind.pk).order_by("-abs_path")
+    for episode in season_eps:
+        if episode.abs_path==ep.abs_path:
+            return curr_episode_ind
+    
+    return None
