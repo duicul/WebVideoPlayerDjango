01f3726d9d688cfbe70d08c22ea4de1296f56ee8
duicul <32304082+duicul@users.noreply.github.com>
2021-12-04 22:52:33 +0200

    Fix autoplay, set timeout before alert

--- WebVideoPlayer/static/js/utils.js
+++ WebVideoPlayer/static/js/utils.js
@@ -409,10 +409,10 @@ function addJumpOnVideoEnd(url){
 let player = videojs('my-video');
 
 player.on('ended', function() {
-  alert('videoended next:'+url);
   setTimeout(function(){
     window.location.href = url;
-}, 5000);
+}, 3000);
+    alert('videoended next:'+url);
   
 });
 }
