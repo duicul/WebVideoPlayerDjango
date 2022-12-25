bf27ba2e34897bd9dfd0f1e787d21a0d18250a2f
duicul <32304082+duicul@users.noreply.github.com>
2022-12-25 23:55:22 +0200

    string list to list

--- WebVideoPlayer/video_player/views.py
+++ WebVideoPlayer/video_player/views.py
@@ -46,8 +46,8 @@ def video(request):
                 mv_db=Movie_db.objects.get(unique_id=uuid)
                 play_src=mv_db.movie_url
                 logger.info("movie subs"+str(mv_db.sub_json))
-                subs = mv_db.sub_json
-                #subs=json.loads(mv_db.sub_json)
+                subs=json.loads(mv_db.sub_json)
+                subs = subs.strip('][').split(',')
                 logger.info("movie subs"+str(subs))
                 subsaux=[]
                 for sub in subs:
@@ -74,8 +74,8 @@ def video(request):
                     nxt_ep_uuid=nxt_ep.unique_id
                 play_src=ep_db.movie_url
                 logger.info("episode subs"+str(ep_db.sub_json))
-                subs=ep_db.sub_json
-                #subs=json.loads(ep_db.sub_json)
+                subs=json.loads(ep_db.sub_json)
+                subs = subs.strip('][').split(',')
                 logger.info("episode subs"+str(subs))
                 subsaux=[]
                 for sub in list(subs):
